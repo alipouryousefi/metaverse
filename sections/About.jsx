ab7038a262ce7b6fb6417e8bacfefd1a52199d6e
Ali Py <a.pouryousefi98@gmail.com>
2023-04-10 15:45:33 +0330

    complete about section

--- sections/About.jsx
+++ sections/About.jsx
@@ -1,8 +1,38 @@
-'use client';
+"use client";
+import { motion } from "framer-motion";
+import { TypingText } from "../components";
+import styles from "../styles";
+import { fadeIn, staggerContainer, textVariant2 } from "../utils/motion";
 
 const About = () => (
-  <section>
-    About section
+  <section className={`${styles.paddings} relative z-10`}>
+    <div className="gradient-02 z-0" />
+    <motion.div
+      variants={staggerContainer}
+      initial="hidden"
+      whileInView="show"
+      viewport={{ once: false, amount: 0.25 }}
+      className={`${styles.innerWidth} mx-auto ${styles.flexCenter} flex-col`}
+    >
+      <TypingText title="| About Metaversus" textStyles="text-center" />
+      <motion.p
+        variants={fadeIn("up", "tween", 0.2, 1)}
+        className="mt-[8px] font-normal sm:text-[32px] text-[20px] text-secondary-white"
+      >
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis est
+        voluptas nihil voluptates, nobis sit tempora quibusdam omnis cupiditate
+        asperiores, laborum quo reprehenderit? Quos sit facilis ad eaque
+        dignissimos quae?Lorem ipsum, dolor sit amet consectetur adipisicing
+        elit. Explicabo aliquam delectus eius est pariatur? Eum eveniet ea
+        accusamus unde obcaecati illo nostrum inventore voluptatem sapiente
+        aspernatur, repudiandae commodi quam corrupti.
+      </motion.p>
+      <motion.img
+        variants={fadeIn("up", "tween", 0.3, 1)}
+        src="/arrow-down.svg"
+        className="w-[18px] h-[28px] object-contain mt-[28px]"
+      ></motion.img>
+    </motion.div>
   </section>
 );
 
--- sections/Hero.jsx
+++ sections/Hero.jsx
@@ -5,7 +5,7 @@ import styles from "../styles";
 import { slideIn, staggerContainer, textVariant } from "../utils/motion";
 
 const Hero = () => (
-  <section className={`${styles.yPaddings} sm:pl-16 pl-6`}>
+  <section className={`${styles.yPaddings} sm:pl-16 pl-6 mx-auto`}>
     <motion.div
       variants={staggerContainer}
       initial="hidden"
@@ -30,13 +30,21 @@ const Hero = () => (
         variants={slideIn("right", "tween", 0.2, 1)}
         className="relative w-full md:-mt-[20px] -mt[12px]"
       >
-        <div className="absolute w-full h-[300px] hero-gradient rounded-tl-[140px] z-0 -top-[30px]">
-          <img
-            src="/cover.png"
-            alt="cover"
-            className="w-full sm:h-[500px] h-[350px] object-cover rounded-tl-[140px] z-10"
-          />
-        </div>
+        <div className="absolute w-full h-[300px] hero-gradient rounded-tl-[140px] z-0 -top-[30px]" />
+        <img
+          src="/cover.png"
+          alt="cover"
+          className="w-full sm:h-[500px] h-[350px] object-cover rounded-tl-[140px] z-10 relative"
+        />
+        <a href="#explore">
+          <div className="w-full flex justify-end sm:-mt-[70px] -mt-[50px] pr-[40px] relative z-10">
+            <img
+              src="/stamp.png"
+              alt="stamp"
+              className="sm:w-[155px] w-[100px] sm:h-[155px] h-[100px] object-contain"
+            />
+          </div>
+        </a>
       </motion.div>
     </motion.div>
   </section>
